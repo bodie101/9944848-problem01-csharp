30e91542bb342291dd7c15627c5783d2dbe69c2e
bodie101 <keysbw@g.toiohomai.ac.nz>
2017-09-04 10:35:56 +1200

    inital commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace problem01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Start the program with Clear();
+            Console.Clear();
+            
+            
+            //End the program with blank line and instructions
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+        }
+    }
+}
